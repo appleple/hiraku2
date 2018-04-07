93bd65867bda0b61cb41b88b49ccbb9b54c6b7b1
Hori Godai <ess_president@me.com>
2018-04-07 16:05:14 +0900

    bumping versions

--- js/hiraku.js
+++ js/hiraku.js
@@ -5,7 +5,7 @@
  * hiraku:
  *   license: MIT (http://opensource.org/licenses/MIT)
  *   author: appleple
- *   version: 2.1.0
+ *   version: 2.1.1
  *
  * component-clone:
  *   maintainers: jongleberry <jonathanrichardong@gmail.com>
